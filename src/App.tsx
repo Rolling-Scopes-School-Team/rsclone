2285f28e49b98ed4fd17c47f0181b89e156da05b
Boris Krasko <boris.krasko@icloud.com>
2021-01-08 00:56:11 +0100

    feat: create GameField component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import TableExample from '@/components/TableExample';
+// import TableExample from '@/components/TableExample';
+import GameField from '@/components/GameField';
 
-const App = (): JSX.Element => <TableExample />;
+const App = (): JSX.Element => <GameField />;
 
 export default App;
---
+++ src/components/GameField/GameField.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-types */
+// import classNames from 'classnames';
+import React from 'react';
+
+// import styles from '@/components/GameField/index.scss';
+
+const GameField: React.FC = () => <div>The field of game will be here!</div>;
+
+export default GameField;
