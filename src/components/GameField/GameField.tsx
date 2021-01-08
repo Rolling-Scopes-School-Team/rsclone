f92d12d4e06bd9676ab3fffa2e93be8e5e3f1850
Boris Krasko <boris.krasko@icloud.com>
2021-01-08 01:56:19 +0100

    feat: add demo content

--- src/components/GameField/GameField.tsx
+++ src/components/GameField/GameField.tsx
@@ -2,8 +2,15 @@
 // import classNames from 'classnames';
 import React from 'react';
 
-// import styles from '@/components/GameField/index.scss';
+import styles from '@/components/GameField/index.scss';
 
-const GameField: React.FC = () => <div>The field of game will be here!</div>;
+const GameField: React.FC = () => (
+  <div className={styles['main']}>
+    <div className={styles['demo-card-l']} />
+    <div className={styles['demo-card-r']} />
+    <div className={styles['title']}>UNO</div>
+    <div className={styles['subtitle']}>minimal with friends</div>
+  </div>
+);
 
 export default GameField;
