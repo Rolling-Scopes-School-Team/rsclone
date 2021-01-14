093ae013e9b6dc2366ba2a34e57e308e62bca109
buiiz <ivantur2@gmail.com>
2021-01-14 02:34:56 +0200

    feat: add intro page

---
+++ src/components/Intro/Intro.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import type { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import UnoLogo from '@/assets/svg/uno-logo.svg';
+import styles from '@/components/Intro/Intro.scss';
+import { NEW_GAME_ROUTE, AUTHORIZE_ROUTE } from '@/utils/consts';
+
+const Intro: FC = (): JSX.Element => {
+  const user = 0; // ! temp
+
+  return (
+    <div className={styles.intro}>
+      <UnoLogo className={styles.logo} />
+      {user ? (
+        <NavLink to={NEW_GAME_ROUTE}>
+          <button type="button" className={styles.btn}>
+            Play
+          </button>
+        </NavLink>
+      ) : (
+        <NavLink to={AUTHORIZE_ROUTE}>
+          <button type="button" className={styles.btn}>
+            Log In
+          </button>
+        </NavLink>
+      )}
+    </div>
+  );
+};
+
+export default Intro;
