839ce056d44bd27d610bb5a98f6be00cfe754153
Deeathwiing <andreykorotkins@gmail.com>
2021-01-15 19:42:53 +0300

    feat: add first step to auth/reg

--- src/components/Authorize/Authorize.tsx
+++ src/components/Authorize/Authorize.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import type { FC } from 'react';
 import { NavLink } from 'react-router-dom';
@@ -8,6 +9,38 @@ import { INTRO_ROUTE } from '@/utils/consts';
 
 const Authorize: FC = (): JSX.Element => {
   const [option, setOption] = useState<'login' | 'registration'>('login');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const registartion = () => {
+    const data = {
+      email,
+      name,
+      password,
+    };
+
+    // eslint-disable-next-line no-void
+    void axios.post('http://localhost:3002/auth/create', data).then(res => {
+      // eslint-disable-next-line no-console
+      console.log(res);
+    });
+  };
+
+  const login = () => {
+    const data = {
+      email,
+      password,
+    };
+
+    // eslint-disable-next-line no-void
+    void axios
+      .post('http://localhost:3002/auth/authUser', data, { withCredentials: true })
+      .then(res => {
+        // eslint-disable-next-line no-console
+        console.log(res);
+      });
+  };
 
   return (
     <div className={styles.authorize}>
@@ -20,9 +53,19 @@ const Authorize: FC = (): JSX.Element => {
       {option === 'login' ? (
         <form className={styles.form}>
           <div className={styles.title}>Login</div>
-          <input className={styles.input} type="email" placeholder="E-mail" />
-          <input className={styles.input} type="password" placeholder="Password" />
-          <button className={styles.btn} type="button">
+          <input
+            onChange={e => setEmail(e.target.value)}
+            className={styles.input}
+            type="email"
+            placeholder="E-mail"
+          />
+          <input
+            onChange={e => setPassword(e.target.value)}
+            className={styles.input}
+            type="password"
+            placeholder="Password"
+          />
+          <button onClick={login} className={styles.btn} type="button">
             Sign In
           </button>
           <button className={styles.link} type="button" onClick={() => setOption('registration')}>
@@ -32,10 +75,25 @@ const Authorize: FC = (): JSX.Element => {
       ) : (
         <form className={styles.form}>
           <div className={styles.title}>Registration</div>
-          <input className={styles.input} type="text" placeholder="Name" />
-          <input className={styles.input} type="email" placeholder="E-mail" />
-          <input className={styles.input} type="password" placeholder="Password" />
-          <button className={styles.btn} type="button">
+          <input
+            onChange={e => setName(e.target.value)}
+            className={styles.input}
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            onChange={e => setEmail(e.target.value)}
+            className={styles.input}
+            type="email"
+            placeholder="E-mail"
+          />
+          <input
+            onChange={e => setPassword(e.target.value)}
+            className={styles.input}
+            type="password"
+            placeholder="Password"
+          />
+          <button onClick={registartion} className={styles.btn} type="button">
             Sign Up
           </button>
           <button className={styles.link} type="button" onClick={() => setOption('login')}>
