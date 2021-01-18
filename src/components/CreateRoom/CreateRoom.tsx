79628ab4ed8faa2399ef449b5f307dd96d9526a9
dmitry_zozulia <z1dima1z@gmail.com>
2021-01-18 20:18:31 +0100

    feat: remove card from create room

--- src/components/CreateRoom/CreateRoom.tsx
+++ src/components/CreateRoom/CreateRoom.tsx
@@ -1,13 +1,11 @@
 import React, { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 
-import Card from '@/components/Card/Card';
 import styles from '@/components/CreateRoom/index.scss';
 import { NEW_GAME_ROUTE, WAIT_FOR_PLAYERS } from '@/utils/consts';
 
 const CreateRoom: FC = (): JSX.Element => (
   <div className={styles.createRoomWrapper}>
-    <Card card="wildDraw4" background="dark" front />
     <div className={styles.createRoom}>
       <form className={styles.form}>
         <h3 className={styles.header}>Create Room</h3>
